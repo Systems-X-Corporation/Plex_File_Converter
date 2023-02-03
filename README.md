4c0ac6769fa9b1ffd3f6eb6185d7964ed1390504
Jessica Hernandez <jessica.hernandez@systems-x.com>
2023-02-03 13:17:00 -0600

    Add project files

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# sx_files_converter
-This repository aims to have the latest desktop tool to convert csv, xls and xlsx into specific output extensions requested by Plex customer 
+# SX-Banorte-Pending-Payments
+
+This application's purpose is to transform extracted data from Plex into a desired input format for Banorte payments
+
+## Installation
+
+It doesn't need any installation process, just click on this [link](https://drive.google.com/file/d/1FTl2cWwMsaAp2iXHeagMVyyG38C1ZreW/view?usp=sharing) to download the exe file and it is ready to be used.
+
+## How to use
+
+1. Go to Plex VP screen Plex 'SX Pending Payments'.
+
+2. Select your search criteria and click the 'Search' button.
+
+3. Download the VP screen output by clicking the 'Export' and 'Export to csv' options in the blue action bar.
+
+4. Double click on the exe file downloaded and mentioned in the Installation step.
+
+5. Enter the 'No. Consecutivo' number.
+
+6. Select the 'SX Pending Payments' csv export file previously downloaded by clicking the '...' button.
+
+7. Once you've selected your file click the 'Convertir' button.
+
+8. After the app transforms the csv file into a txt file, the 'Documents' folder will pop up with the new file in it.
+
+
+## Created By
+
+Systems-X
---
+++ main.py
@@ -0,0 +1,87 @@
+import PySimpleGUI as sg
+import pandas as pd
+import os
+from datetime import date
+
+sg.theme("LightGreen")
+layout = [[sg.T("")], [sg.Text("No. Consecutivo:  "), sg.Input(default_text="001", key="-CONC-", enable_events=True)],
+          [sg.T("")],
+          [sg.Text("Archivo: "), sg.Input(readonly=True),
+           sg.FileBrowse(button_text=". . .", key="-IN-")], [sg.T("")], [sg.Button("Convertir")]]
+
+###Building Window
+window = sg.Window('Banorte Payments by Systems-X', layout, size=(500, 180), element_justification='center')
+
+today = date.today()
+try:
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Exit":
+            break
+        if event == '-CONC-' and values['-CONC-'] and (values['-CONC-'][-1] not in ('0123456789') or len(
+                values['-CONC-']) > 3):
+            window['-CONC-'].update(values['-CONC-'][:-1])
+        elif event == "Convertir":
+            if values["-IN-"].lower().endswith(('.csv')) == True:
+                if values['-CONC-']:
+
+                    df = pd.read_csv(values["-IN-"])
+
+                    df['Operacion'] = df['Operacion'].fillna('')
+                    df['Clave ID'] = df['Clave ID'].fillna('')
+                    df['Cuenta Origen'] = df['Cuenta Origen'].fillna('')
+                    df['Cuenta Destino'] = df['Cuenta Destino'].fillna('')
+                    df['Importe'] = df['Importe'].fillna(0)
+                    df['Importe'] = df['Importe'].replace(",", "", regex=True)
+                    df['Importe'] = df['Importe'].astype(float)
+                    df['Importe'] = (df['Importe']*100).astype(int)
+                    df['Referencia'] = df['Referencia'].fillna('')
+                    df['Descripcion'] = df['Descripcion'].fillna('')
+                    df['Moneda Origen'] = df['Moneda Origen'].fillna('1')
+                    df['Moneda Destino'] = df['Moneda Destino'].fillna('1')
+                    df['RFC Ordenante'] = df['RFC Ordenante'].fillna('')
+                    df['IVA'] = df['IVA'].fillna(0)
+                    df['Email Beneficiario'] = df['Email Beneficiario'].fillna('')
+                    df['Fecha Aplicacion'] = df['Fecha Aplicacion'].fillna('')
+                    df['Instruccion Pago'] = df['Instruccion Pago'].fillna('')
+
+                    export_Payments = ''
+
+                    for index, row in df.iterrows():
+                        export_Payments += str(row['Operacion']).rjust(2, '0') + \
+                                           str(row['Clave ID']).ljust(13) + \
+                                           str(row['Cuenta Origen']).rjust(20, '0') + \
+                                           str(row['Cuenta Destino']).rjust(20, '0') + \
+                                           str(row['Importe']).rjust(14, '0') + \
+                                           str(row['Referencia']).rjust(10, '0') + \
+                                           str(row['Descripcion']).ljust(30) + \
+                                           str(row['Moneda Origen']).ljust(1, '1') + \
+                                           str(row['Moneda Destino']).ljust(1, '1') + \
+                                           str(row['RFC Ordenante']).ljust(13) + \
+                                           str(row['IVA']).rjust(14, '0') + \
+                                           str(row['Email Beneficiario']).ljust(39) + \
+                                           str(row['Fecha Aplicacion']).replace('/', '').ljust(8) + \
+                                           str(row['Instruccion Pago']).ljust(70) + '\n'
+
+                    with open(os.path.join(os.path.expanduser('~'), 'Documents',
+                                           'PP012345' + today.strftime("%Y%m%d") + values["-CONC-"] + '.txt'),
+                              "w") as export_Txt:
+                        export_Txt.write(export_Payments)
+
+                    sg.Popup('Archivo creado con éxito', keep_on_top=True, modal=True)
+
+                    path = os.path.expanduser('~/Documents')
+                    path = os.path.realpath(path)
+                    os.startfile(path)
+                else:
+                    sg.Popup('No se ingresó un No. Consecutivo', keep_on_top=True, modal=True)
+
+            elif values["-IN-"] == "":
+                sg.Popup('No se seleccionó un archivo', keep_on_top=True, modal=True)
+
+            else:
+                sg.Popup('Archivo inválido', keep_on_top=True, modal=True)
+except:
+    sg.Popup('Ocurrió un error. Intente de nuevo', keep_on_top=True, modal=True)
+
+window.close()
